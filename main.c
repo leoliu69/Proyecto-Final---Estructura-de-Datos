4b428805d356c9b1a65c7104d158e1319408ffd6
leoliu69 <patricio.quispe.a@mail.pucv.cl>
2025-11-13 13:08:18 -0300

    added 'mallar curricular & prereq validations'

--- main.c
+++ main.c
@@ -3,7 +3,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main() {
+    AppState* state createAppState() ;
+    char opcion;
+    printf("========================================\n");
+    printf(" SISTEMA DE AVANCE CURRICULAR\n");
+    printf("  Ingeniería Civil Informática\n - PUCV\n");
+    printf("  (Actualizado 2025)\n");
+    printf("========================================\n\n");
 
-void cargarMalla(AppState* state, const char* csv_path);
-void actualizarSemestre(AppState* state, int num_semestre);
-void inscribirAsignaturas(AppState* state, int num_semestre)
+    printf("¿Desea cargar una malla curricular? (S/N): ");
+    char cargar;
+    scanf(" %c", &cargar);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) 
+
+    if (cargar == 'S' || cargar == 's') {
+        limpiarPantalla();
+        printf("Ingrese el nombre del archivo (debe estar en la carpeta 'data'):\n");
+        printf("Ejemplo: mi_malla.csv\n");
+        printf(">");
+        
+        char nombreArchivo[256];
+        scanf("%255s", nombreArchivo);
+        
+    }
+}
