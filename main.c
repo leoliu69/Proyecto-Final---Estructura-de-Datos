c9656d8c195ae3ba2efd7359b8d6afa9dd31464b
Miguel <miguel.carvajal.c@mail.pucv.cl>
2025-11-13 12:54:55 -0300

    prueba

--- main.c
+++ main.c
@@ -4,15 +4,5 @@
 #include <stdlib.h>
 
 int main{
-    AppState* state = createAppState();
-    char opcion;
-
-    printf("========================================\n");
-    printf("  SISTEMA DE AVANCE CURRICULAR\n");
-    printf("  Ingenieria Civil Informatica - PUCV\n");
-    printf("  (actualizado 2025)\n");
-    printf("========================================\n\n");
-
-    printf("¿Desea cargar una malla curricular? (S/N): ");
-    char cargar;
+    
 }
