553d2e06dbc6a35de7e3d86d0a7c49993e7d5015
leoliu69 <patricio.quispe.a@mail.pucv.cl>
2025-11-13 13:05:43 -0300

    function protoypes for malla curricular

--- main.c
+++ main.c
@@ -2,3 +2,8 @@
 #include "tdas/extra.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+
+void cargarMalla(AppState* state, const char* csv_path);
+void actualizarSemestre(AppState* state, int num_semestre);
+void inscribirAsignaturas(AppState* state, int num_semestre)
