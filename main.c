f643bff08970ba6211f735f295505a8134b7d33b
Your Name <you@example.com>
2025-11-13 12:11:12 -0300

    main cambio

--- main.c
+++ main.c
@@ -2,3 +2,16 @@
 #include "tdas/extra.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+int main{
+    AppState* state = createAppState();
+    char opcion;
+
+    printf("========================================\n");
+    printf("  SISTEMA DE AVANCE CURRICULAR\n");
+    printf("  Ingenieria Civil Informatica - PUCV\n");
+    printf("  (actualizado 2025)\n");
+    printf("========================================\n\n");
+
+    printf("¿Desea cargar una malla curricular? (S/N): ");
+}
